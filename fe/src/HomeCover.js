ef0f7883e400534abfb18b4fc8c24bb7bd43aab3
pimpareshubham <127371609+pimpareshubham@users.noreply.github.com>
2024-03-09 12:26:10 +0530

    Update HomeCover.js

--- fe/src/HomeCover.js
+++ fe/src/HomeCover.js
@@ -3,7 +3,7 @@ import React from 'react';
 const HomeCover = () => {
     return (
         <>
-            <div className="container-fluid">
+            <div className="container-fluid cover-image">
 
 
                 <h1 className="e-l">Ecommerce</h1>
